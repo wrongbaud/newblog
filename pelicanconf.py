dae227488d4dc0e0d9655da1af27538b1f027a92
wrongbaud <wrongbaud@gmail.com>
2024-05-26 14:42:09 -0400

    Testing

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,6 @@
 AUTHOR = 'VoidStar Security'
 SITENAME = 'VoidStar Security Blog'
-SITEURL = ''
+SITEURL = 'https://wrongbaud.github.io/newblog'
 PLUGIN_PATHS = ['/home/wrongbaud/projects/vss/blog-resources/pelican-plugins', ]
 PLUGINS = ['i18n_subsites','pelican_just_table','tag_cloud']
 JINJA_ENVIRONMENT = {
